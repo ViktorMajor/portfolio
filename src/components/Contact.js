ac55a14f6335982b87e2f002d0c833d4b5189898
Major Viktor <majorviktor95@gmail.com>
2023-07-04 21:57:16 +0200

    add additional responsive style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,46 @@
 import React from "react";
 import "../styles/Contact.css";
 
+const handleResumeClick = () => {
+  window.open(process.env.PUBLIC_URL + "/resume.pdf", "_blank");
+};
+
 const Contact = () => {
   return (
     <React.Fragment>
       <div className="contact-card">
         <h1 className="contact-header">Contact Me</h1>
         <div className="contact-info">
-          <p><strong>Email:</strong> majorviktor95@gmail.com</p>
-          <p><strong>Phone:</strong> +36 30 84 999 84</p>
           <p>
-            <strong>LinkedIn:</strong> 
-            <a href="https://www.linkedin.com/in/viktor-major-559b2a250/" target="_blank" rel="noopener noreferrer">
+            <strong>Email:</strong> majorviktor95@gmail.com
+          </p>
+          <p>
+            <strong>Phone:</strong> +36 30 84 999 84
+          </p>
+          <p>
+            <strong>LinkedIn:</strong>
+            <a
+              href="https://www.linkedin.com/in/viktor-major-559b2a250/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Viktor Major
             </a>
           </p>
           <p>
             <strong>GitHub:</strong>
-            <a href="https://github.com/viktormajor" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/viktormajor"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               viktormajor
             </a>
           </p>
-          <button onClick={() => download()}>Töltsd le az önéletrajzom</button>
+
+          <button className="resume-button" onClick={handleResumeClick}>
+            Dowvload my resume!
+          </button>
         </div>
       </div>
     </React.Fragment>
